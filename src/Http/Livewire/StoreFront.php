862795c8c50e86732e36696446952721f18bd075
Nguyễn Trường Khánh Duy <khanhduy2610@gmail.com>
2024-04-15 09:43:56 +0700

    init

---
+++ src/Http/Livewire/StoreFront.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dtaa\Http\Livewire;
+
+use Livewire\Component;
+
+class StoreFront extends Component
+{
+    public function render()
+    {
+
+        return view('dtaa::livewire.store-front');
+    }
+}
