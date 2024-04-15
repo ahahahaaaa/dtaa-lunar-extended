1313c6adf1281a069351561536596dfd30046c36
Nguyễn Trường Khánh Duy <khanhduy2610@gmail.com>
2024-04-15 18:44:40 +0700

    update

--- routes/includes/storefront.php
+++ routes/includes/storefront.php
@@ -3,5 +3,5 @@
 Route::group([
     'middleware' => 'can:dtaa:manage-storefront',
 ], function () {
-    Route::get('/', StoreFront::class)->name('dtaa.storefront.index');
+    Route::get('/', \DtaaLunarExtended\Http\Livewire\StoreFront::class)->name('dtaa.storefront.index');
 });
